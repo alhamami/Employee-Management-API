31d832c7a72ee92dc238f77ba3ded213947885a1
alhamami <jalalalhamami@gmail.com>
2025-01-25 16:19:23 +0300

    Updating EmployeeControllerTest

--- src/test/java/com/digivisions/employee_management/controller/EmployeeControllerTest.java
+++ src/test/java/com/digivisions/employee_management/controller/EmployeeControllerTest.java
@@ -1,4 +1,4 @@
-package com.digivisions.employee_management.api;
+package com.digivisions.employee_management.controller;
 
 import com.digivisions.employee_management.DTO.EmployeeDTO;
 import com.digivisions.employee_management.DTO.EmployeeResponseDTO;
