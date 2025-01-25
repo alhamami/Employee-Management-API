21d5d86fbb6fdf97ea3a692f472dc1ffcd26c183
alhamami <jalalalhamami@gmail.com>
2025-01-25 16:19:35 +0300

    Updating EmailService

--- src/main/java/com/digivisions/employee_management/service/EmailService.java
+++ src/main/java/com/digivisions/employee_management/service/EmailService.java
@@ -1,6 +1,5 @@
 package com.digivisions.employee_management.service;
 
-import com.digivisions.employee_management.api.EmployeeController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
